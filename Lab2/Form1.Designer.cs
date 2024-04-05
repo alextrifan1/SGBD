b484072f3a55c137ea1bb171877b25929b2f69c8
Alex Trifan <98693246+alextrifan1@users.noreply.github.com>
2024-04-05 22:09:21 +0300

    lab2

---
+++ Lab2/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Update = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Pink;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 76);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(464, 139);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.Color.Pink;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 307);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(464, 150);
+            this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tabel Parinte";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Update
+            // 
+            this.Update.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.Update.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Update.Location = new System.Drawing.Point(12, 486);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(151, 36);
+            this.Update.TabIndex = 6;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = false;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 268);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 27);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Tabel Fiu";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Pink;
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(571, 76);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(506, 139);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button4.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(147, 21);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 44);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Add";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button3.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(272, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 44);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button2.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(388, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 44);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(27, 99);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(27, 73);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(27, 47);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(27, 21);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label7.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(3, 99);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(18, 22);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "4";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(3, 73);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 22);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 22);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 22);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "1";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(1089, 595);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Lab2/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Xml;
+using System.Diagnostics;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cs = new SqlConnection("Data Source=DESKTOP-3AU86S9\\SQLEXPRESS;Initial Catalog=SGBDLAB;Integrated Security=True");
+
+        public static string parentTable = ConfigurationManager.AppSettings.Get("parentTable");
+        public static string childTable = ConfigurationManager.AppSettings.Get("childTable");
+        public static string parentPrimaryKey = ConfigurationManager.AppSettings.Get("parentPrimaryKey");
+        public static string childForeignKey = ConfigurationManager.AppSettings.Get("childForeignKey");
+        public static string childPrimaryKey = ConfigurationManager.AppSettings.Get("childPrimaryKey");
+
+        DataSet ds = new DataSet();
+
+        SqlDataAdapter parentDataAdapter = new SqlDataAdapter();
+        SqlDataAdapter childDataAdapter = new SqlDataAdapter();
+
+        BindingSource parentBindingSource = new BindingSource();
+        BindingSource childBindingSource = new BindingSource();
+
+        //SqlCommandBuilder parentBuilder = new SqlCommandBuilder();
+        //SqlCommandBuilder childBuilder = new SqlCommandBuilder();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeData();
+        }
+        //afisare tabel 1
+        private void InitializeData()
+        {
+            try
+            {
+                parentDataAdapter = new SqlDataAdapter("SELECT * FROM " + parentTable, cs);
+                childDataAdapter = new SqlDataAdapter("SELECT * FROM " + childTable, cs);
+
+                SqlCommandBuilder parentBuilder = new SqlCommandBuilder(parentDataAdapter);
+                SqlCommandBuilder childBuilder = new SqlCommandBuilder(childDataAdapter);
+
+                //ds.Clear();
+                parentDataAdapter.Fill(ds, parentTable);
+                childDataAdapter.Fill(ds, childTable);
+
+                DataColumn parentColumn = ds.Tables[parentTable].Columns[parentPrimaryKey];
+                DataColumn childColumn = ds.Tables[childTable].Columns[childForeignKey];
+
+                DataRelation relation = new DataRelation("fk_parent_child", parentColumn, childColumn);
+                ds.Relations.Add(relation);
+
+                parentBindingSource.DataSource = ds;
+                parentBindingSource.DataMember = parentTable;
+
+                childBindingSource.DataSource = parentBindingSource;
+                childBindingSource.DataMember = "fk_parent_child";
+
+                dataGridView1.DataSource = parentBindingSource;
+                dataGridView2.DataSource = childBindingSource;
+
+                //dataGridView1.DataSource = ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                parentDataAdapter.Update(ds, parentTable);
+                childDataAdapter.Update(ds, childTable);
+                MessageBox.Show("Updated with succes!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                cs.Close();
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            InitializeData(); 
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+         
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //butonul pentru afisare din tabela parinte in tabela fiu
+        private void button1_Click(object sender, EventArgs e){}
+
+        private void panel1_Paint(object sender, PaintEventArgs e){}
+
+        //buton de update
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (childTable == "ModelMasina")
+            {
+                string idMasina = textBox2.Text;
+                string idEchipa = textBox3.Text;
+                string numeMasina = textBox4.Text;
+                string idFurnizorMotor = textBox5.Text;
+                try
+                {
+                    // parcurgem toate randurile din tabelul copil
+                    DataRow rowToUpdate = null;
+                    foreach (DataRow row in ds.Tables[childTable].Rows)
+                    {
+                        if (row[childPrimaryKey].ToString() == idMasina) // daca am gasit randul de sters il stergem si ne oprim
+                        {
+                            rowToUpdate = row;
+                            break;
+                        }
+                    }
+
+                    if (rowToUpdate != null)
+                    {
+                        rowToUpdate[childForeignKey] = idEchipa;
+                        rowToUpdate["numeMasina"] = numeMasina;
+                        rowToUpdate["idFurnizorMotor"] = idFurnizorMotor;
+
+                        // Update the database using the data adapter to reflect the changes
+                        childDataAdapter.Update(ds, childTable);
+
+                        // Commit the changes to the DataSet
+                        ds.AcceptChanges();
+
+                        MessageBox.Show("Row updated successfully!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("No row found to update.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+            if (childTable == "CascaSofer")
+            {
+                string idCasca = textBox2.Text;
+                string producator = textBox3.Text;
+                string culoarePrimara = textBox4.Text;
+                string idSofer = textBox5.Text;
+                try
+                {
+                    // parcurgem toate randurile din tabelul copil
+                    DataRow rowToUpdate = null;
+                    foreach (DataRow row in ds.Tables[childTable].Rows)
+                    {
+                        if (row[childPrimaryKey].ToString() == idCasca) // daca am gasit randul de sters il stergem si ne oprim
+                        {
+                            rowToUpdate = row;
+                            break;
+                        }
+                    }
+
+                    if (rowToUpdate != null)
+                    {
+                        rowToUpdate[childForeignKey] = idSofer;
+                        rowToUpdate["producator"] = producator;
+                        rowToUpdate["culoarePrimara"] = culoarePrimara;
+                        rowToUpdate["idSofer"] = idSofer;
+
+                        // Update the database using the data adapter to reflect the changes
+                        childDataAdapter.Update(ds, childTable);
+
+                        // Commit the changes to the DataSet
+                        ds.AcceptChanges();
+
+                        MessageBox.Show("Row updated successfully!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("No row found to update.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }   
+            }
+        }
+
+        //buton de delete
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string idToBeDeleted = textBox2.Text;
+            try
+            {
+                // parcurgem toate randurile din tabelul copil
+                DataRow rowToDelete = null;
+                foreach (DataRow row in ds.Tables[childTable].Rows)
+                {
+                    if (row[childPrimaryKey].ToString() == idToBeDeleted) // daca am gasit randul de sters il stergem si ne oprim
+                    {
+                        rowToDelete = row;
+                        break;
+                    }
+                }
+
+                if (rowToDelete != null)
+                {
+                    // stergem randul din data set
+                    rowToDelete.Delete();
+
+                    // Update the database using the data adapter to reflect the changes
+                    childDataAdapter.Update(ds, childTable);
+
+                    // Commit the changes to the DataSet
+                    ds.AcceptChanges();
+
+                    MessageBox.Show("Row deleted successfully!");
+                }
+                else
+                {
+                    MessageBox.Show("No row found to delete.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        //buton de add
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (childTable == "ModelMasina")
+            {
+                string idMasina = textBox2.Text;
+                string idEchipa = textBox3.Text;
+                string numeMasina = textBox4.Text;
+                string idFurnizorMotor = textBox5.Text;
+                try
+                {
+                    DataRow newRow = ds.Tables[childTable].NewRow();
+                    newRow[childPrimaryKey] = idMasina;
+                    newRow[childForeignKey] = idEchipa;
+                    newRow["numeMasina"] = numeMasina;
+                    newRow["idFurnizorMotor"] = idFurnizorMotor;
+
+                    ds.Tables[childTable].Rows.Add(newRow);
+
+                    childDataAdapter.Update(ds, childTable);
+
+                    ds.AcceptChanges();
+
+                    MessageBox.Show("Row added successfully!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+            if (childTable == "CascaSofer")
+            {
+                string idCasca = textBox2.Text;
+                string producator = textBox3.Text;
+                string culoarePrimara = textBox4.Text;
+                string idSofer = textBox5.Text;
+                try
+                {
+                    DataRow newRow = ds.Tables[childTable].NewRow();
+                    newRow[childPrimaryKey] = idCasca;
+                    newRow[childForeignKey] = idCasca;
+                    newRow["producator"] = producator;
+                    newRow["culoarePrimara"] = culoarePrimara;
+                    newRow["idSofer"] = idSofer;
+
+                    ds.Tables[childTable].Rows.Add(newRow);
+
+                    childDataAdapter.Update(ds, childTable);
+
+                    ds.AcceptChanges();
+
+                    MessageBox.Show("Row added successfully!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
